b6a71cbb8a8beab035e1348ef3960dada7c30148
Michael <Michael.ginalick@gmail.com>
2015-05-17 11:00:27 -0500

    checkin migrations created

--- libraryapp/app/models/checkin.rb
+++ libraryapp/app/models/checkin.rb
@@ -1,2 +1,6 @@
 class Checkin < ActiveRecord::Base
+  attr_accessible :user_id, :book_id, :checkin_date
+
+  belongs_to :user
+  belongs_to :book
 end
--- libraryapp/db/migrate/20150514030116_create_checkins.rb
+++ libraryapp/db/migrate/20150514030116_create_checkins.rb
@@ -2,6 +2,10 @@ class CreateCheckins < ActiveRecord::Migration
   def change
     create_table :checkins do |t|
 
+      t.integer     :user_id
+      t.integer     :book_id
+      t.date        :checkin_date
+
       t.timestamps
     end
   end
