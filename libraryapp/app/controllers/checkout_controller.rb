da00344471d9e860f282e813455479a6119d0014
Michael <Michael.ginalick@gmail.com>
2015-05-21 23:29:36 -0500

    helper methods added for due and start dates for checkouts

--- libraryapp/app/controllers/checkout_controller.rb
+++ libraryapp/app/controllers/checkout_controller.rb
@@ -1,23 +1,23 @@
 class CheckoutController < ApplicationController
   
-	def new_checkout
-		@user = User.find_by(session[:user_id])
-    	@book = Book.find_by(params[:id])
-    	@checkout = Checkout.create(:user_id => @user.id, :book_id => @book.id)
-
-    	if @checkout.save
-            @book.update(checked_out?: true)
-    		respond_to do |format|
-    		  format.html {redirect_to user_path(session[:user_id])}
-    	    end
-        else    
-    		flash[:notice] = "Book was not checked out, please try again"
-    		redirect_to book_path(@book) 
-    	end
-	end
+	def create
+    end
+	   
 
     def show
-        redirect_to()
+        @user = User.find(session[:user_id])
+        @book = Book.find(params[:id])
+        @checkout = Checkout.create(:user_id => @user, :book_id => @book)
+        if @checkout.save
+            @book.update(checked_out?: true)
+            respond_to do |format|
+              format.html {redirect_to user_path(session[:user_id])}
+            end
+        else    
+            flash[:notice] = "Book was not checked out, please try again"
+            redirect_to book_path(@book) 
+        end
+        
     end
 
 
