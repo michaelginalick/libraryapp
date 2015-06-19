94a18fc9da15f9645661f7109abc5c519a60f1f2
Michael <Michael.ginalick@gmail.com>
2015-06-19 18:50:47 -0500

    spacing clean up

--- libraryapp/app/controllers/checkin_controller.rb
+++ libraryapp/app/controllers/checkin_controller.rb
@@ -6,14 +6,14 @@ class CheckinController < ApplicationController
     	@checkin = Checkin.create(:user_id => @user.id, :book_id => @book.id, :checkin_date => checkin_date)
 
         if @checkin.save
-            @book.update(checked_out?: false)
+           @book.update(checked_out?: false)
             respond_to do |format|
               format.html {redirect_to user_path(session[:user_id])}
               flash[:notice] = "Thanks!"
             end
         else    
-            flash[:notice] = "Book was not checked out, please try again"
-            redirect_to book_path(@book) 
+           flash[:notice] = "Book was not checked out, please try again"
+           redirect_to book_path(@book) 
         end
 	end
 
