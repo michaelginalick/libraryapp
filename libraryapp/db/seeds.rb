e912a6115026b5b7e3731e83b0340e1791dd4234
Michael <Michael.ginalick@gmail.com>
2015-06-20 20:02:36 -0500

    added csv file for seed

--- libraryapp/db/seeds.rb
+++ libraryapp/db/seeds.rb
@@ -1,10 +1,14 @@
+require 'csv'
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# CSV.foreach('db/vehicles.csv', :headers => true) do |record|
+#   Book.create(title: record["title"], author: record["author"], genre: record["genre"], good_reads_id: record["good_reads_id"], checked_out?: record["checked_out?"])
+# end
+
+
 
   admin = Admin.create(email: "admin@gmail.com", password: "mag42c84")
 
