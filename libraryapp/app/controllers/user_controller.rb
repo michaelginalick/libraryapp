d456f8b9a0477a1aaf2813e79fe733146cbc27ce
Michael <Michael.ginalick@gmail.com>
2015-06-20 23:10:45 -0500

    button styling

--- libraryapp/app/controllers/user_controller.rb
+++ libraryapp/app/controllers/user_controller.rb
@@ -2,14 +2,6 @@ class UserController < ApplicationController
 
   skip_before_filter :verify_authenticity_token
 
-
-  #As a user I want to
-  #to be able to view all books
-  #to be able to search for books
-  #to be able to view an individual book
-  #to be able to checkout a book
-  #to be able to checkin a book I have checked out
-
   def index
     if session[:user_id] != nil
       @user = User.find(session[:user_id])
